6ae2cce6e3bdffa5198d1c5160959dfe11d836a3
AmiManafli <amina.manafli@gmail.com>
2020-06-19 19:12:14 +0200

    Stage Two completed: Evaluate difficulty based on words per sentence metric

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -2,13 +2,33 @@ package readability;
 import java.util.Scanner;
 
 public class Main {
+    static String wordEnd = "\\s+|\\p{Z}|\\p{Space}|\\p{Blank}";
+    static String sentenceEnd = "[.!?]\\s|[.!?]\\p{Blank}|[.!?]\\p{Space}";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        if (text.length() > 100) {
+        double wordAvg = analyzeText(text);
+        if (wordAvg > 10) {
             System.out.println("HARD");
         } else {
             System.out.println("EASY");
         }
+    }
+
+    private static double analyzeText(String text) {
+        String[] sentences = text.split(sentenceEnd);
+        double wordCount = 0;
+        for (String sentence : sentences) {
+            wordCount += analyzeSentence(sentence);
+        }
+//        System.out.printf("The total number of words is %d in %d sentences\n",
+//                            wordCount, sentences.length);
+        return wordCount / sentences.length;
+    }
+
+    private static int analyzeSentence(String sentence) {
+        String[] words = sentence.split(wordEnd);
+        return words.length;
     }
 }
